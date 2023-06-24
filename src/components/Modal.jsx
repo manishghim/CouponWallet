652c41bfb035fce6b4e74e89c9f9135c974c3715
manishghim <manishghimire2073@gmail.com>
2023-06-24 18:40:54 +0545

    create modal for the coupon code

---
+++ src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import CloseButton from "@images/close_btn.svg"
+
+import "@sass/components/_modal.scss"
+
+const Modal = (props) => {
+	const {isOpen, closeModal, children} = props
+
+  return isOpen ? (
+    <div className='Modal'>
+			<div className='Modal__body'>
+				<div className='Modal__backgroun'></div>
+				<img className='Modal__close_btn' src={CloseButton} alt="closeButton" onClick={closeModal} />
+				{children}
+			</div>
+    </div>
+  ) : null
+}
+
+export default Modal
--- src/views/Coupon/index.jsx
+++ src/views/Coupon/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CouponCard from '@components/CouponCard'
+import Modal from '@components/Modal'
 
 import Cinema from "@images/cinema.svg"
 import Plane from "@images/plane.svg"
@@ -9,12 +10,30 @@ import Goldstar from "@images/goldstar.svg"
 import "@sass/views/_coupon.scss";
 
 const Coupon = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const openModal = () => {
+		setIsOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsOpen(false)
+	}
+
   return (
     <div className='Coupon'>
-        <div className='Coupon__row'><CouponCard img={Cinema} points={200} discount={5} text="ANY CINEMAS" /></div>
-        <div className='Coupon__row'><CouponCard img={Plane} points={500} discount={5} text="PLANE TICKET" /></div>
-        <div className='Coupon__row'><CouponCard img={BigMart} points={1000} discount={5} text="BIG MART" /></div>
-        <div className='Coupon__row'><CouponCard img={Goldstar} points={1200} discount={5} text="GOLDSTAR PRODUCT" /></div>
+			<div className='Coupon__container'>
+				<div className='Coupon__row' onClick={openModal}><CouponCard img={Cinema} points={200} discount={5} text="ANY CINEMAS" /></div>
+				<div className='Coupon__row' onClick={openModal}><CouponCard img={Plane} points={500} discount={10} text="PLANE TICKET" /></div>
+				<div className='Coupon__row' onClick={openModal}><CouponCard img={BigMart} points={1000} discount={12} text="BIG MART" /></div>
+				<div className='Coupon__row' onClick={openModal}><CouponCard img={Goldstar} points={1200} discount={15} text="GOLDSTAR PRODUCT" /></div>
+			</div>
+			<Modal isOpen={isOpen} closeModal={closeModal}>
+				<div className='Coupon__modal-content'>
+						<h2>Code</h2>
+						<p className='Coupon__modal-code'>3456</p>
+				</div>
+			</Modal>
     </div>
   )
 }
